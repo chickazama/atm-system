978658f51328568776f02d0c307591263d6be0fe
Matthew Hope <matt@matthewhope.co.uk>
2023-06-26 18:30:09 +0100

    Implement main menu

--- src/system.c
+++ src/system.c
@@ -2,8 +2,16 @@
 #include "menu.h"
 #include "system.h"
 
+#define BUF_LEN 64
+
 int run_main_menu(void) {
     main_menu();
     printf("\n> ");
+    char buf[BUF_LEN];
+    if ( fgets(buf, BUF_LEN, stdin) == NULL ) {
+	perror("fgets");
+	return -1;
+    }
+    printf("%s\n", buf);
     return 0;
 }
