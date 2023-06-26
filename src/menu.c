a5064ac09683248dac9538922108cef3254a79b0
Matthew Hope <matt@matthewhope.co.uk>
2023-06-27 00:13:16 +0100

    Change print_profile_menu display

--- src/menu.c
+++ src/menu.c
@@ -15,6 +15,8 @@ void print_main_menu(void) {
 void print_profile_menu(struct user* u) {
     printf("%s\n", title);
     printf("\n=== Welcome '%s' ===\n", u->username);
-    printf("\n[1] - My Accounts\n");
-    printf("\n[2] - Log Out\n");
+    printf("\n[1] - Withdraw\n");
+    printf("\n[2] - Deposit\n");
+    printf("\n[3] - My Accounts\n");
+    printf("\n[4] - Log Out\n");
 }
