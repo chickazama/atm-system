d719e34ad53ec0c07ed1c87fa365a265403c1686
Matthew Hope <matt@matthewhope.co.uk>
2023-06-26 22:15:33 +0100

    Add test username_exists

---
+++ src/auth.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include "auth.h"
+
+int username_exists(char* username) {
+    printf("Username submitted: %s\n", username);
+    if ( strcmp(username, "test") == 0 )
+	    return 1;
+    return 0;
+}
---
+++ src/auth.h
@@ -0,0 +1,5 @@
+#ifndef AUTH_H
+#define AUTH_H
+#endif
+
+int username_exists(char*);
