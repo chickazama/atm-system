548444abeab10d24c57fe940966c8ad78d1e7b8a
Matthew Hope <matt@matthewhope.co.uk>
2023-06-26 22:17:04 +0100

    Implement login scaffold

--- src/main.c
+++ src/main.c
@@ -5,20 +5,23 @@
 int main(int argc, char* argv[]) {
     int selection;
     do {
-	selection = run_main_menu();
+	    selection = main_menu();
     } while (selection <= 0);
+
     switch (selection) {
-	case 1:
-	printf("\nLoading register menu...\n");
-	break;
-	case 2:
-	printf("\nLoading login menu...\n");
-	break;
-	case 3:
-	printf("\nThank you for using the ATM. Goodbye!\n");
-	break;
-	default:
-	exit(EXIT_FAILURE);
+        case 1:
+        printf("\nLoading register menu...\n");
+        break;
+        case 2:
+        //printf("\nLoading login menu...\n");
+        int res = login_menu();
+        printf("result: %d\n", res);
+        break;
+        case 3:
+        printf("\nThank you for using the ATM. Goodbye!\n");
+        break;
+        default:
+        exit(EXIT_FAILURE);
     }
     return EXIT_SUCCESS;
 }
