09a3f6c0bdd97805fc931ccdd3d5a2b210628754
Matthew Hope <matt@matthewhope.co.uk>
2023-06-26 18:06:48 +0100

    Add main menu function

---
+++ src/menu.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "menu.h"
+
+void main_menu(void) {
+    printf("=== 01Founders ATM System\n");
+    printf("\nWelcome to the 01Founders ATM.\n");
+    printf("\n[1] - Register\n");
+    printf("\n[2] - Login\n");
+    printf("\n[3] - Exit\n");
+}
