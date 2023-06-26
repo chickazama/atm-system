d779453c80a5f4b4df2abc9ccfe8b13ceafd444a
Matthew Hope <matt@matthewhope.co.uk>
2023-06-26 18:22:09 +0100

    Scaffold system run functions

---
+++ src/system.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "menu.h"
+#include "system.h"
+
+int run_main_menu(void) {
+    main_menu();
+    printf("\n> ");
+    return 0;
+}
---
+++ src/system.h
@@ -0,0 +1,6 @@
+#ifndef SYSTEM_H
+#define SYSTEM_H
+#endif
+
+int run_main_menu(void);
+
