d85065681b62b822b269532de5d3bab66a047ebc
Matthew Hope <matt@matthewhope.co.uk>
2023-06-26 18:52:43 +0100

    Switch on menu selection

--- src/main.c
+++ src/main.c
@@ -7,5 +7,18 @@ int main(int argc, char* argv[]) {
     do {
 	selection = run_main_menu();
     } while (selection <= 0);
+    switch (selection) {
+	case 1:
+	printf("\nLoading register menu...\n");
+	break;
+	case 2:
+	printf("\nLoading login menu...\n");
+	break;
+	case 3:
+	printf("\nThank you for using the ATM. Goodbye!\n");
+	break;
+	default:
+	exit(EXIT_FAILURE);
+    }
     return EXIT_SUCCESS;
 }
