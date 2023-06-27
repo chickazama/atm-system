b51aa8bb72cfa21e2d2eaa205b2d70badbea1ef5
Matthew Hope <matt@matthewhope.co.uk>
2023-06-27 09:31:10 +0100

    Scaffold record struct

--- src/atm.h
+++ src/atm.h
@@ -2,9 +2,19 @@
 #define ATM_H
 
 struct user {
+	// long long id;
 	char username[20];
 	char password[20];
 };
+
+struct record {
+	int id;
+	//date
+	char country[20];
+	int accountNumber;
+	double balance;
+	char type[20];
+}
 
 #endif
 
