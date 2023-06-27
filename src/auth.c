3bdd17a6af258d727438b5bfd7b8c8efd9d97c36
Matthew Hope <matt@matthewhope.co.uk>
2023-06-27 14:00:18 +0100

    Fix auth return values

--- src/auth.c
+++ src/auth.c
@@ -5,13 +5,13 @@
 
 int user_exists(struct user* u) {
     if ( strcmp(u->username, "test") == 0 )
-	    return -1;
+	    return 1;
     return 0;
 }
 
 int check_password(struct user* u) {
     if ( strcmp(u->password, "password") == 0 )
-	    return -1;
+	    return 1;
     return 0;
 }
 
